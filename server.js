afd8bd5559c0b8da28b9bc92b3f66d23b011912e
kazuma_ns <kazuma_ns@kazmac.local>
2016-08-20 06:58:28 +0900

    hoge

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-app.listen(process.env.PORT || 8000);
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+  res.send('hogehoge');
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port, function(){console.log("Listening on " + port)});
